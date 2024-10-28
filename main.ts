7f2bed5f455a6d722b7a3e1e3f6184ecd225ac88
Nicolás Flores Alvear <146827258+NicolasFlores7e7@users.noreply.github.com>
2024-10-28 15:43:26 +0100

    Acabado

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-basic.forever(function () {
-	
+let secondDice = 0
+let dice = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    secondDice = randint(1, 6)
+    if (secondDice < receivedNumber) {
+        basic.showIcon(IconNames.Sad)
+    } else if (secondDice > receivedNumber) {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("STRING")
+    basic.showString("SEND")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    dice = randint(1, 6)
+    radio.sendNumber(dice)
+    basic.showNumber(dice)
 })
